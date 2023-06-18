fa59ff53eb36c31279d6ac4271a2cd1564860862
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-06-18 10:26:56 +0530

    Time: 70 ms (14.34%), Space: 42.1 MB (39.95%) - LeetHub

---
+++ 2749-promise-time-limit/2749-promise-time-limit.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        return new Promise(async (resolve, reject) => {
+        const tt = setTimeout(() => {
+            reject("Time Limit Exceeded");
+        }, t);
+
+        try {
+            const result = await fn(...args);
+            resolve(result);
+        } catch(err) {
+            reject(err);
+        }
+        clearTimeout(tt);
+        });
+    }
+};
+
