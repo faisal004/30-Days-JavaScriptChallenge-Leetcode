c4405692cd019dc6e0e5cfdabb7bcbad0c2a7caf
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-06-22 10:30:34 +0530

    Time: 292 ms (99.16%), Space: 88.9 MB (99.76%) - LeetHub

---
+++ 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    
+    const res = [];
+    let i = 0, j = 0;
+    while(i < arr1.length && j < arr2.length) {
+        if (arr1[i].id < arr2[j].id) {
+            res.push(arr1[i++]);
+        } else if (arr1[i].id === arr2[j].id){
+            res.push(Object.assign(arr1[i++], arr2[j++]));
+        } else {
+            res.push(arr2[j++]);
+        }
+    }
+    while(i < arr1.length) {
+        res.push(arr1[i++]);
+    }
+    while(j < arr2.length) {
+        res.push(arr2[j++]);
+    }
+    return res;
+};
